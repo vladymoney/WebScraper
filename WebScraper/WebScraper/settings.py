c6a3183f85e907f76d8a3dcf4edf017cb4d7426f
Vladimir Nankov <102216547+vladymoney@users.noreply.github.com>
2023-07-21 14:52:56 +0300

    Added simple CSS

--- WebScraper/WebScraper/settings.py
+++ WebScraper/WebScraper/settings.py
@@ -103,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -118,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
